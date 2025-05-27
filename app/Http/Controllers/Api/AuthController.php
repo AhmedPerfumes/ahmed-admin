237457b8eeb85cecd5e7216adccc21366bb3563f
“Arshad <arshad@ahmedalmaghribi.com>
2025-05-27 12:42:57 +0400

    added whatsapp

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -201,7 +201,7 @@ class AuthController extends Controller
             return response()->json($validator->errors());
         }        
 
-        $otp = rand(1111, 9999);
+        $otp = rand(111111, 999999);
 
         $ch = curl_init();
 
@@ -219,6 +219,47 @@ class AuthController extends Controller
             echo 'Error:' . curl_error($ch);die;
         }
         curl_close ($ch);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://waba.myinboxmedia.in/api/sendwaba',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'{
+            "ProfileId": "MIM2400074",
+            "APIKey": "#JpXt4fbMCFj",
+            "MobileNumber": 971'.ltrim($request->mobile, $request->mobile[0]).',
+            "templateName": "websiteauthentication",
+            "Parameters": [
+                '.$otp.'      
+            ],
+            "HeaderType": "Text",
+            "Text": "",
+            "MediaUrl": "",
+            "Latitude": 0,
+            "Longitude": 0,
+            "isTemplate": "true",
+            "ButtonOrListJSON": "",
+            "SubClientCode": "",
+            "HeaderParameter": "",
+            "CTAButtonURLParameter":"",
+            "CTAButtonURLParameter2" : ""
+        }',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        // echo $response;
 
         $mobile_verification = MobileVerification::where('phone', $request->mobile)->get();
 
