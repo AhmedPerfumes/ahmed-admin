e20abdaf67f32ec5ce7eb81bf89eea9a423ba193
Saad Edroos <saadedroossaad@gmail.com>
2025-08-21 17:03:23 +0400

    Add view and discount %

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Promotion extends Model
+use Botble\Base\Models\BaseModel;
+class Promotion extends BaseModel
 {
     use HasFactory;
 
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,13 @@ Route::prefix('admin/ecommerce/dynamic')->group(function () {
     Route::delete('/admin/dynamic-section/{id}', [DynamicSectionController::class, 'destroy'])->name('dynamic-section.destroy'); 
 });
 
+Route::get('promotions', [PromotionController::class, 'index'])->name('promotions.index');
 Route::get('promotions/create', [PromotionController::class, 'create'])->name('promotions.create');
+Route::get('promotions/data', [PromotionController::class, 'data'])->name('promotions.data');
 Route::post('promotions', [PromotionController::class, 'store'])->name('promotions.store');
+Route::get('promotions/{promotion}/edit', [PromotionController::class, 'edit'])->name('promotions.edit');
+Route::put('promotions/{promotion}', [PromotionController::class, 'update'])->name('promotions.update');
+Route::delete('/promotions/bulk-delete', [PromotionController::class, 'bulkDelete'])->name('promotions.bulkDelete');
+Route::delete('promotions/{promotion}', [PromotionController::class, 'destroy'])->name('promotions.destroy');
 
 
