ba7b9e4ba5af6b9f0dab00bc354f4e4e8d685220
saood ghare <saoodghare0809@gmail.com>
2025-10-09 11:19:27 +0400

    small change

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -1720,7 +1720,7 @@ class OrderController extends Controller
             "return"=> "http://localhost/ahmed-admin/public/api/payTabsPaymentRedirect?order_number=".base64_encode($order->code)
         ];
 
-        $PROFILE_ID = config('paytabs.profile_id');;
+        $PROFILE_ID = config('paytabs.profile_id');
         // $PROFILE_ID = 48353;
         $SERVER_KEY = config('paytabs.server_key');
         // $SERVER_KEY = 'S6JNLMDMDL-HZM2DZDHLN-GW2NZ6DKK2';
